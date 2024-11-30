ea9830e09f96e2e4918a95db324375b1759654a0
yusuke suzuki <92585953+Amorphouse@users.noreply.github.com>
2024-11-30 20:48:51 +0900

    frontendを差し替え 既存資産を削除し、vite + quasarで新しくPJ作成

---
+++ frontend/vite.config.ts
@@ -0,0 +1,28 @@
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+
+    // @quasar/plugin-vite options list:
+    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
+    quasar({
+      sassVariables: fileURLToPath(
+        new URL('./src/quasar-variables.sass', import.meta.url)
+      )
+    })
+  ]
+});
