64b72c62fcf3f8e849c65bb9696e0ace346a8890
yusuke suzuki <92585953+Amorphouse@users.noreply.github.com>
2024-12-01 00:13:20 +0900

    fix: :bug: buildできるように色々改修

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
 import ChallengeForm from '@/components/ChallengeForm.vue'
 </script>
 
@@ -12,7 +11,6 @@ import ChallengeForm from '@/components/ChallengeForm.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <!-- <HelloWorld msg="Vite + Vue" /> -->
   <ChallengeForm />
 </template>
 
